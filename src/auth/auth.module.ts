d1b26c671fcb63dca77ffdd3098955ba16c7a42a
Noam Honig <noam.honig@gmail.com>
2022-05-27 11:18:58 +0300

    extended the time for the jwt for testing purposes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '60m' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
